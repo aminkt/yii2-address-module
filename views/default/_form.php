c4959008cfd32062f353d3a5bb9aced0c75ebd9c
saghar mojdehi <saghar.mojdehi@gmail.com>
2018-01-27 00:32:32 +0330

    map

--- views/default/_form.php
+++ views/default/_form.php
@@ -1,5 +1,8 @@
 <?php
 /** @var \yii\web\View $this */
+
+use yii\web\JsExpression;
+
 /** @var \saghar\address\models\Address $model */
 
 $url = \yii\helpers\Url::to(['/address/default/get-cities'], true);
@@ -14,7 +17,7 @@ $this->title = "ثبت آدرس جدید"
 
     echo $form->field($model, 'cityId')->widget(\kartik\select2\Select2::className(), [
         'data' => $cities,
-        'options' => ['placeholder' => 'دسته را انتخاب کنید ...'],
+        'options' => ['placeholder' => 'شهر را انتخاب کنید ...'],
         'pluginOptions' => [
             'allowClear' => true
         ]
@@ -25,6 +28,33 @@ $this->title = "ثبت آدرس جدید"
 
     <?= $form->field($model, 'zipCode')->textInput() ?>
 
+    <?= $form->field($model, 'latitude')->hiddenInput(['id' => 'us2-lat'])->label(false) ?>
+    <?= $form->field($model, 'longitude')->hiddenInput(['id' => 'us2-lon'])->label(false) ?>
+
+    Location: <input type="text" id="us2-address" style="width: 200px"/>
+    <br>
+
+    <?php
+    echo \pigolab\locationpicker\LocationPickerWidget::widget([
+        'key' => 'AIzaSyBjFztI9oEaunJToOiAQ9lSS8Bfbre3BNo',	// require , Put your google map api key
+        'options' => [
+            'style' => 'width: 100%; height: 400px', // map canvas width and height
+        ] ,
+        'clientOptions' => [
+            'location' => [
+                'latitude'  => 37.28083300 ,
+                'longitude' => 49.58305600,
+            ],
+            'addressFormat' => 'street_number',
+            'inputBinding' => [
+                'latitudeInput'     => new JsExpression("$('#us2-lat')"),
+                'longitudeInput'    => new JsExpression("$('#us2-lon')"),
+                'locationNameInput' => new JsExpression("$('#us2-address')")
+            ],
+
+        ]
+    ]);
+    ?>
 
     <?= \yii\helpers\Html::submitButton($model->isNewRecord ? "ذخیره" : "ویرایش", [
         'class' => 'btn btn-primary'
